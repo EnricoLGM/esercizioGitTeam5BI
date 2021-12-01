51aa45cd22dae5a8064080bb2496049b295306ed
SimonVentura <vsimone474@gmail.com>
2021-12-01 12:32:43 +0100

    "toast petrini"

--- app/src/main/java/com/example/eserciziogitteam5bi/MainActivity.java
+++ app/src/main/java/com/example/eserciziogitteam5bi/MainActivity.java
@@ -5,10 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText testo;
+        int duration = Toast.LENGHT_LONG;
+        Toast toast = Toast.makeText(context, testo.getText(), duration);
+        toast.show();
+
     }
+
 }
