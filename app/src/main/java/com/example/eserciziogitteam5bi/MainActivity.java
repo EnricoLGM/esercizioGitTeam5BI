4e8df09013311c7ec2038c7df1cc9cf1f960904f
LeoXy03 <lbrenci91@gmail.com>
2021-12-01 12:35:00 +0100

    Toast Brenci

--- app/src/main/java/com/example/eserciziogitteam5bi/MainActivity.java
+++ app/src/main/java/com/example/eserciziogitteam5bi/MainActivity.java
@@ -5,10 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void messaggio(View v){
+        int duration = Toast.LENGTH_LONG;
+        Toast toast = Toast.makeText(context,"ciao sono brenci",duration);
+        toast.show();
+    }
+
 }
